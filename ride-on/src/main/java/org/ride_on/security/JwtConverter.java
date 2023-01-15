f1d6689834a45effbe7c545c8d5a32a8c60f2e1a
Matt Heine <102334843+mattheine@users.noreply.github.com>
2023-01-14 21:11:55 -0700

    Added : Spring token-based-security

---
+++ ride-on/src/main/java/org/ride_on/security/JwtConverter.java
@@ -0,0 +1,71 @@
+package org.ride_on.security;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class JwtConverter {
+
+    private Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private final String ISSUER = "ride-on";
+    private final int EXPIRATION_MINUTES = 20160;
+    private final int EXPIRATION_MILLIS = EXPIRATION_MINUTES * 60 * 1000;
+
+    public String getTokenFromUser(UserDetails user) {
+
+        String authorities = user.getAuthorities().stream()
+                .map(i -> i.getAuthority())
+                .collect(Collectors.joining(","));
+
+        // 3. Use JJWT classes to build a token.
+        return Jwts.builder()
+                .setIssuer(ISSUER)
+                .setSubject(user.getUsername())
+                .claim("authorities", authorities)
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_MILLIS))
+                .signWith(key)
+                .compact();
+    }
+
+    public UserDetails getUserFromToken(String token) {
+
+        if (token == null || !token.startsWith("Bearer ")) {
+            return null;
+        }
+
+        try {
+
+            Jws<Claims> jws = Jwts.parserBuilder()
+                    .requireIssuer(ISSUER)
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token.substring(7));
+
+            String username = jws.getBody().getSubject();
+            String authStr = (String) jws.getBody().get("authorities");
+
+            List<SimpleGrantedAuthority> roles = Arrays.stream(authStr.split(","))
+                    .map(r -> new SimpleGrantedAuthority(r))
+                    .collect(Collectors.toList());
+
+            return new User(username, username, roles);
+
+        } catch (JwtException e) {
+
+            System.out.println(e);
+        }
+
+        return null;
+    }
+}
---
+++ ride-on/src/main/java/org/ride_on/security/SecurityConfig.java
@@ -0,0 +1,61 @@
+package org.ride_on.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+public class SecurityConfig {
+
+    private final JwtConverter converter;
+
+    public SecurityConfig(JwtConverter converter) {
+        this.converter = converter;
+    }
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, AuthenticationConfiguration authconfig) throws Exception {
+
+        http.csrf().disable();
+
+        http.cors();
+
+        http.authorizeRequests()
+                .antMatchers("/authenticate").permitAll()
+                .antMatchers("/refresh_token").authenticated()
+                .antMatchers(HttpMethod.GET,
+                        "/home").permitAll()
+                .antMatchers(HttpMethod.GET,
+                        "/registered-user", "/registered-user/*").hasAnyAuthority("USER", "ADMIN")
+                .antMatchers(HttpMethod.POST,
+                        "/registered-user/*").hasAnyAuthority("USER", "ADMIN")
+                .antMatchers(HttpMethod.GET,
+                        "/car/*").hasAnyAuthority("USER", "ADMIN")
+                .antMatchers(HttpMethod.POST,
+                        "/car").hasAnyAuthority("USER", "ADMIN")
+                .antMatchers(HttpMethod.GET,
+                        "/rider/*").hasAnyAuthority("USER", "ADMIN")
+                .antMatchers(HttpMethod.PUT,
+                        "/rider/*").hasAnyAuthority("USER", "ADMIN")
+                .antMatchers("/**").denyAll()
+                .and()
+                .addFilter(new JwtRequestFilter(authenticationManager(authconfig), converter))
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+}
